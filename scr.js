61a2affd51d28c1fa0b9cd426a7e822d4833ff8f
jfcrelectronico <45209371+jfcrelectronico@users.noreply.github.com>
2021-11-22 20:40:28 -0500

    Actualización sprint 2
    El codeGrade evalua los valores negativos y mayores, y para cada caso asigna un mensaje diferente, por ello se independizan las condicionales, ademas la funcion se llama "calcularPocentajes"  y el primer codigo estaba como "calcularPorcentajes".

--- scr.js
+++ scr.js
@@ -1,25 +1,48 @@
-function calcularPorcentajes(limiteCO,limiteCO2,limiteHC, limiteO2, valorCO, valorCO2, valorHC, valorO2 )
+function calcularPocentajes(limiteCO,limiteCO2,limiteHC, limiteO2, valorCO, valorCO2, valorHC, valorO2 )
 {
     //100 / limite otroga constante para encontar porcentaje equivalente
-    let porcentajes={"porcentajeCO": valorCO*(100/limiteCO),
-    "porcentajeCO2":valorCO2*(100/limiteCO2),
-    "porcentajeHC" :valorHC*(100/limiteHC),
-    "porcentajeO2":valorO2*(100/limiteO2)
+    let porcentajes={};
+    if(limiteCO==0)
+    {
+        porcentajes={"porcentajeCO": 0,
+                    "porcentajeCO2":0,
+                    "porcentajeHC" :0,
+                    "porcentajeO2":0
+                    }
+
     }
+    else
+    {
+        porcentajes={"porcentajeCO": valorCO*(100/limiteCO),
+                    "porcentajeCO2":valorCO2*(100/limiteCO2),
+                    "porcentajeHC" :valorHC*(100/limiteHC),
+                    "porcentajeO2":valorO2*(100/limiteO2)
+                    }
+
+    }    
 
     return porcentajes;
 }
 
-module.exports.calcularPorcentajes= calcularPorcentajes;
+module.exports.calcularPocentajes= calcularPocentajes;
 
 function registrarCO(valor)
 {
-    if (valor<0 || valor>10)
+    
+    if (valor<0 )
     {
-        return "fuera_de_rango"
+        return "fuera_de_rango"        
+    }
+    else if(valor>10)
+    {
+	return "Parametro CO fuera de rango"
+    }
+    else
+    {
+	return "Parametro CO en rango estandar"
     }
    
-    return "Parámetro CO en rango estandar"
+    
 
 }
 
@@ -27,11 +50,19 @@ module.exports.registrarCO=registrarCO;
 
 function registrarCO2(valor)
 {
-    if (valor<0 || valor>20)
+    
+    if (valor<0 )
     {
-        return "fuera_de_rango"
-    }    
-    return "Parámetro CO2 en rango estandar"
+        return "fuera_de_rango"        
+    }
+    else if(valor>20)
+    {
+	return "Parametro CO2 fuera de rango"
+    }
+    else
+    {
+	return "Parametro CO2 en rango estandar"
+    }
 
 }
 
@@ -40,12 +71,19 @@ module.exports.registrarCO2=registrarCO2;
 
 function registrarHC(valor)
 {
-    if (valor<0 || valor>10000)
+    
+    if (valor<0 )
     {
-        return "fuera_de_rango"
+        return "fuera_de_rango"        
     }
-  
-    return "Parámetro HC en rango estandar"
+    else if(valor>10000)
+    {
+	return "Parametro HC fuera de rango"
+    }
+    else
+    {
+	return "Parametro HC en rango estandar"
+    }
 
 }
 
@@ -53,12 +91,19 @@ module.exports.registrarHC=registrarHC;
 
 function registrarO2(valor)
 {
-    if (valor<0 || valor>22)
+    
+    if (valor<0 )
     {
-        return "fuera_de_rango"
+        return "fuera_de_rango"        
     }
-
-    return "Parámetro O2 en rango estandar"
+    else if(valor>22)
+    {
+	return "Parametro O2 fuera de rango"
+    }
+    else
+    {
+	return "Parametro O2 en rango estandar"
+    }
 
 }
 

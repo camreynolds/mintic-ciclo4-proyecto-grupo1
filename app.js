d0d5b40d67d2ea196f5dc904d9cdafba3fe38c79
Carlos Muñoz <camr.personal@gmail.com>
2021-12-05 16:09:13 -0500

    Creado.

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 // Importar los módulos.
 const express = require('express');
+const bodyParser = require('body-parser');
+const { stringify } = require('querystring');
+const { json } = require('stream/consumers');
 
 // Configuración del servidor.
 const app   = express();
-const port  = 3300;
+const port  = 3000;
 
 
-app.get("/prueba", (req,res) => {
-    let phrase = "It's alive!";
-    res.send(phrase);
+// async function limites(req,res){
+//     const resultado = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/limits")
+//     .then( data => console.log(data))
+//     .catch( (e) => console.error("Error"))
+// };
+
+app.get("/prueba", async (req,res) => {
+    const respuesta = await fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/limits");
+    
 });
+
+// app.get("/prueba", async (req,res) => {
+//     fetch("https://misiontic2022upb.vercel.app/api/emission-measurement/limits")
+//         .then( data => {
+//            console.log(data); 
+//         })
+//         // .then( data => console.log(data))
+//         .catch( (e) => console.error("Error"))
+// });
+
+
 
 app.listen(port, () => {
     console.log(`API ejecutandose en el puerto ${port}.`);
 });
+
